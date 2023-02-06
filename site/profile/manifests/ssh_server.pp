72cf6626592e387d7d5bd3cd74f6cecaa249d649
Guiteguit <121866187+Guiteguit@users.noreply.github.com>
2023-02-06 16:38:59 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,15 +1,15 @@
 class profile::ssh_server {
-	package {'openssh-server':
-		ensure => present,
-	}
-	service { 'sshd':
-		ensure => 'running',
-		enable => 'true',
-	}
-	ssh_authorized_key { 'root@master.puppet.vm':
-		ensure => present,
-		user => 'root',
-		type => 'ssh-rsa',
-		key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDDyqydLciH2PWSCPBTXlNAXO/HA6cW3PWTlLtAld7ybuEs915movMvxyLi2nQb6mWioys0YMEmbp9cAMcLpC4QT8G0aqazU3Z6oFZKiL+ybGbckJVMYqNdwAjCzpF5Lu34+Mj3ryv2ZZHbTR56JCargZiXrerI74dDfukP7S4ZzonyNKcT/tXuoX4LHcek8d22OToVK9hBHOF5HsX3ehgcZw8/1FY254YVCvXRldORXBZN/8pn9vbm3Qv2CEbwgDjdyWwxcFZR7B2lZjGYrQGXFyBummlPPmgA6bJ8kY0BLCN2e+8vYLarsmRakWvxgbAC4wKjrB8JJvqGBH2ntbgR',
-	}  
+  package {'openssh-server':
+    ensure => present,
+  }
+  service { 'sshd':
+    ensure => 'running',
+    enable => 'true',
+  }
+  ssh_authorized_key { 'root@master.puppet.vm':
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDDyqydLciH2PWSCPBTXlNAXO/HA6cW3PWTlLtAld7ybuEs915movMvxyLi2nQb6mWioys0YMEmbp9cAMcLpC4QT8G0aqazU3Z6oFZKiL+ybGbckJVMYqNdwAjCzpF5Lu34+Mj3ryv2ZZHbTR56JCargZiXrerI74dDfukP7S4ZzonyNKcT/tXuoX4LHcek8d22OToVK9hBHOF5HsX3ehgcZw8/1FY254YVCvXRldORXBZN/8pn9vbm3Qv2CEbwgDjdyWwxcFZR7B2lZjGYrQGXFyBummlPPmgA6bJ8kY0BLCN2e+8vYLarsmRakWvxgbAC4wKjrB8JJvqGBH2ntbgR',
+  }  
 }
