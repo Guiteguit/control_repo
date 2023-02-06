88b08b7b02621dee9c91910905fe9dd3a24016bb
Guiteguit <121866187+Guiteguit@users.noreply.github.com>
2023-02-06 16:27:17 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,17 +5,20 @@ node default {
     owner   => 'root',
   }
 }
-node 'master.puppet.vm' {
+
+node 'master.puppet.vm': {
   include role::master_server
   file {'/root/README':
     ensure => file,
     content => “Welcome to ${fqdn}”,
     owner => 'root',
-  }
+  }  
 }
+
 node 'minetest.puppet.vm': {
   include role::minecraft_server
 }
+
 node /^web/ { 
   include role::app_server
 }
